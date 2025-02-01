d556d028a20d9ba89c8b00a9ee78b505b773b050
CA.1 <mohammad92007@gmail.com>
2025-02-01 14:57:20 +0400

    Add files via upload

--- Server/server.lua
+++ Server/server.lua
@@ -1,1 +1,15 @@
+local QBCore = exports['qb-core']:GetCoreObject()
 
+QBCore.Functions.CreateUseableItem("car_tracker", function(source, item)
+    local Player = QBCore.Functions.GetPlayer(source)
+    if Player.PlayerData.job.name == Config.AllowedJob then
+        TriggerClientEvent('pd_tracker:useTracker', source)
+        Player.Functions.RemoveItem("car_tracker", 1)
+    else
+        TriggerClientEvent('QBCore:Notify', source, "ليس لديك صلاحية لاستخدام هذا!", "error")
+    end
+end)
+
+RegisterNetEvent('pd_tracker:placeTracker', function(vehicleNetId)
+    TriggerClientEvent('pd_tracker:placeTracker', -1, vehicleNetId)
+end)

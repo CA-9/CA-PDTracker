6e874537057ea0110cb145cdf22a709d08f6931d
CA.1 <mohammad92007@gmail.com>
2025-02-01 14:58:07 +0400

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,6 @@
+Config = {}
+
+Config.TrackerBlip = 161
+Config.TrackerColour = 1
+Config.AllowedJob = "police"
+Config.Proximity = 5.0 -- Distance to detect vehicles near the player
---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'CA'
+description 'Police Car Tracker Script'
+version '1.0.0'
+
+shared_script 'config.lua'
+
+client_script 'client.lua'
+server_script 'server.lua'
+
+lua54 'yes'
